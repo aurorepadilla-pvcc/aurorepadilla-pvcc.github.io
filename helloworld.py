4476c33ae2607ae0095ce4e33e30b845637e7198
aurorepadilla-pvcc <159857445+aurorepadilla-pvcc@users.noreply.github.com>
2024-03-18 22:06:51 -0400

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,13 @@
-# Name: Aurore Padilla
-# Prog Purpose: Hello World
-
-print("Hello World! This is my first Python program.")
-print("I can program in HTML, which is awesome.")
-print("However, HTML cannot do arithmetic.")
-print("But Python can: \n\t Add \n\t Substract \n\t Multiply \n\t Divide")
-print("and lots of other things!")
+Python 3.12.2 (v3.12.2:6abddd9f6a, Feb  6 2024, 17:02:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: /Users/aurorepadilla/Desktop/PYTHON/helloworld.py ==========
+Hello World! This is my first Python program.
+I can program in HTML, which is awesome.
+However, HTML cannot do arithmetic.
+But Python can: 
+	 Add 
+	 Substract 
+	 Multiply 
+	 Divide
+and lots of other things!
